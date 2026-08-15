c0401c61bab513b8e6311d12ca70bf27f0e41da1
Randy Ainsworth <randyainsworth.789@gmail.com>
2026-08-15 14:02:05 -0700

    added Scalar API ref and OData set up

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.OData;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//OData setup
+builder.Services
+    .AddControllers()
+    .AddOData(
+    options => options.Select().Filter().OrderBy().Count()
+    //.Expand().SetMaxTop()
+    );
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
