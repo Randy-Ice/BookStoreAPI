56533aa6c21d8c905e79f74d5e3fd66784f9399c
Randy Ainsworth <randyainsworth.789@gmail.com>
2026-08-15 14:52:20 -0700

    DbContext ready to go

---
+++ Data/Database.cs
@@ -0,0 +1,21 @@
+﻿using BookStoreAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreAPI.Data
+{
+    public class Database : DbContext
+    {
+        public Database(DbContextOptions<Database> options) : base(options)
+        {
+
+        }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Format> Formats { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
