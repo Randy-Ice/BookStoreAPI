a8958be282e047bcacc63f70d13af9b820259ea8
Randy Ainsworth <randyainsworth.789@gmail.com>
2026-08-15 15:29:39 -0700

    idk what changed

--- Models/Review.cs
+++ Models/Review.cs
@@ -1,15 +1,16 @@
-﻿using System.Text.Json.Serialization;
-
-namespace BookStoreAPI.Models
+﻿namespace BookStoreAPI.Models
 {
     public class Review
     {
         public Guid Id { get; set; }
         public string FullName { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedAt { get; set; }
-        [JsonIgnore]
-        public List<Book> Books { get; set; } = new();
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public Guid BookId { get; set; }
+        public Book Book { get; set; }
+        //[JsonIgnore]
+        //public List<Book> Books { get; set; } = new();
 
     }
 }
