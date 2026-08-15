7dad042307af37c8e38cb51fd42d5aadf0a2898b
Randy Ainsworth <randyainsworth.789@gmail.com>
2026-08-15 15:38:38 -0700

    added image field

--- Models/Book.cs
+++ Models/Book.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string? CoverImage { get; set; }
         public short NumberOfPages { get; set; }
         public decimal Price { get; set; }
         public DateTime ReleaseDate { get; set; }
